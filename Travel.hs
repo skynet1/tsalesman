1437595c4020d99d5610358aedd12ea676cf9ab4
skynet1 <thecrazymancan@gmail.com>
2020-05-29 11:29:01 -0500

    Added travelTree.

--- Travel.hs
+++ Travel.hs
@@ -1,6 +1,8 @@
 module Travel where
 
 import Types
+import Data.Tree
+import Graph
 
 travel :: Graph -> Double -> Node -> [Node] -> [(Double, Node, [Node])]
 travel (Graph edges) currentDist startNode destNodes = do
@@ -8,3 +10,9 @@ travel (Graph edges) currentDist startNode destNodes = do
     let filterNodes node nodes = filter (\x -> x /= node) nodes
     let ret = map (\(Edge _ stop dist) -> (currentDist + dist , stop, (filterNodes stop destNodes))) connectedNodes
     ret
+
+travelTree :: Graph -> Double -> Node -> [Node] -> Tree (Double, Node, [Node])
+travelTree graph dist start destNodes = do
+    let sub = map (\(dist2, start2, destNodes2) -> travelTree graph dist2 start2 destNodes2 ) (travel graph dist start destNodes)
+    let root = Data.Tree.Node {rootLabel = (dist, start, destNodes), subForest = sub}
+    root
