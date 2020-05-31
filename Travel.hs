b4a3a0030f94598dd98c7c030ec97708dcecafc2
skynet1 <thecrazymancan@gmail.com>
2020-05-31 07:54:07 -0500

    Added keeping track of the path taken so far to the binary tree.

--- Travel.hs
+++ Travel.hs
@@ -4,14 +4,14 @@ import Types
 import Data.Tree
 import Graph
 
-travel :: Graph -> Double -> Node -> [Node] -> [(Double, Node, [Node])]
-travel (Graph edges) currentDist startNode destNodes = do
-    let connectedNodes = filter (\(Edge start stop dist) -> start == startNode) edges
+travel :: Graph -> Double -> [Node] -> [Node] -> [(Double, [Node], [Node])]
+travel (Graph edges) currentDist startNodes destNodes = do
+    let connectedNodes = filter (\(Edge start stop dist) -> start == (startNodes !! 0) ) edges
     let filterNodes node nodes = filter (\x -> x /= node) nodes
-    let ret = map (\(Edge _ stop dist) -> (currentDist + dist , stop, (filterNodes stop destNodes))) connectedNodes
+    let ret = map (\(Edge _ stop dist) -> (currentDist + dist , (stop : startNodes), (filterNodes stop destNodes))) connectedNodes
     ret
 
-travelTree :: Graph -> Double -> Node -> [Node] -> Tree (Double, Node, [Node])
+travelTree :: Graph -> Double -> [Node] -> [Node] -> Tree (Double, [Node], [Node])
 travelTree graph dist start destNodes = do
     let sub = map (\(dist2, start2, destNodes2) -> travelTree graph dist2 start2 destNodes2 ) (travel graph dist start destNodes)
     let root = Data.Tree.Node {rootLabel = (dist, start, destNodes), subForest = sub}
