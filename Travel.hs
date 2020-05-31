0a5cf80fc06c33b17854e21e16cde33b3f9fe49b
skynet1 <thecrazymancan@gmail.com>
2020-05-31 08:00:48 -0500

    Added getting the graph path.

--- Travel.hs
+++ Travel.hs
@@ -2,6 +2,7 @@ module Travel where
 
 import Types
 import Data.Tree
+import Data.List
 import Graph
 
 travel :: Graph -> Double -> [Node] -> [Node] -> [(Double, [Node], [Node])]
@@ -16,3 +17,13 @@ travelTree graph dist start destNodes = do
     let sub = map (\(dist2, start2, destNodes2) -> travelTree graph dist2 start2 destNodes2 ) (travel graph dist start destNodes)
     let root = Data.Tree.Node {rootLabel = (dist, start, destNodes), subForest = sub}
     root
+
+getPath :: Graph -> Int -> Node -> [Node] -> [Node]
+getPath graph numRoutes start destNodes = do
+    let l = levels (travelTree graph 0.0 [start] destNodes)
+    let samples = foldr (++) [] l
+    let samples2 = filter (\(_, _, z) -> z == []) samples
+    let samples3 = map (\(x, y, _) -> (x, y)) samples2
+    let sorted = sort samples3
+    let (_, final) = sorted !! 0
+    reverse final
