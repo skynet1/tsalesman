59ee9db3506880ea08279330744b2751487b0b33
skynet1 <thecrazymancan@gmail.com>
2020-06-05 14:17:17 -0500

    Added command line flags.

--- tsalesman.hs
+++ tsalesman.hs
@@ -5,20 +5,46 @@ import Graph
 import Travel
 import Output
 import System.Environment
+import System.Console.GetOpt
 import Data.List.Split
 import Data.List
 import System.IO
+
+data Option = Delimiter Char | StartNode Node | Destinations [Node]
+
+options :: [OptDescr Option]
+options = [
+    Option ['d'] ["delimiter"] (ReqArg (\(x :_) -> Delimiter x  ) ":") "Set the delimiter between node names, and distances",
+    Option ['s'] ["start"] (ReqArg (\x -> StartNode x) "s0") "Set the start node.",
+    Option ['n'] ["nodes"] (ReqArg (\x -> Destinations (endBy "," x)) "s0") "Comma seperated list of nodes to visit." ]
+
+getDelim :: [Option] -> Char
+getDelim [] = ':'
+getDelim ((Delimiter c) : _) = c
+getDelim (_ : rest) = getDelim rest
+
+getStartNode :: [Option] -> Node
+getStartNode [] = "s0"
+getStartNode ((StartNode x) : _) = x
+getStartNode (_ : rest) = getStartNode rest
+
+getDests :: [Option] -> [Node]
+getDests [] = ["s0"]
+getDests ((Destinations x) : _) = x
+getDests (_ : rest) = getDests rest
 
 main :: IO ()
 main = do
     args <- getArgs
     inputStr <- getContents
+    let (opts, _, _) = getOpt RequireOrder options args
     hSetBuffering stdout (BlockBuffering Nothing)
-    let graph = getGraph inputStr ((args !! 1) !! 0)
-    let start = (args !! 2)
-    let nodes = endBy "," (args !! 0)
-    let numRoutes = 10000
+    let delim = getDelim opts
+    let graph = getGraph inputStr delim
+    let start = getStartNode opts
+    let nodes = getDests opts
+    let numRoutes = 10000000
     let path = getPath graph numRoutes start nodes
-    let outputStr = path2str path ((args !! 1) !! 0)
+    let outputStr = path2str path delim
     putStr outputStr
 
